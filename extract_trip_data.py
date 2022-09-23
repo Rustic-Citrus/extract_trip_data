392894a757ecf76cf59c75a3bfc14efa66e5b53f
Rustic-Citrus <mrcurtis.english@gmail.com>
2022-09-23 08:10:10 -0300

    commit

--- extract_trip_data.py
+++ extract_trip_data.py
@@ -10,16 +10,22 @@ num_strings_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 for file in glob.glob("./*.pdf"):
     raw = parser.from_file(file)
-    total = raw["content"][(raw["content"].find("Total") + 9):(raw["content"].find("Total") + 14)]
-    total = total.replace(",", ".")
-    total = float(total)
+    total = raw["content"][(raw["content"].find("Total") + 9):(raw["content"].find("Total") + 14)] # finding the total values
+    total = total.replace(",", ".") # replacing decimal commas with points
+    total = float(total) # converting totals from strings into floats
     totals.append(total)
-    if raw["content"][(raw["content"].find("2022")) - 6] in num_strings_list:
-        datetime_object = datetime.strptime(raw["content"][(raw["content"].find("2022") - 6):(raw["content"].find("2022") + 10)].strip(),"%d/%m/%Y %H:%M")
+    if raw["content"][(raw["content"].find("2022")) - 6] in num_strings_list: # finding the dates and times
+        datetime_object = datetime.strptime(
+            raw["content"][(raw["content"].find("2022") - 6):(raw["content"].find("2022") + 10)].strip(),
+            "%d/%m/%Y %H:%M"
+            )
     else:
-        datetime_object = datetime.strptime(raw["content"][(raw["content"].find("2022") - 5):(raw["content"].find("2022") + 10)].strip(),"%d/%m/%Y %H:%M")
-    dates.append(datetime_object.strftime("%d/%m/%Y"))
-    times.append(datetime_object.strftime("%H:%M"))
+        datetime_object = datetime.strptime(
+            raw["content"][(raw["content"].find("2022") - 5):(raw["content"].find("2022") + 10)].strip(),
+            "%d/%m/%Y %H:%M"
+            )
+    dates.append(datetime_object.strftime("%d/%m/%Y")) # extracting the date
+    times.append(datetime_object.strftime("%H:%M")) # extracting the time
 
 df = pd.DataFrame({"total": totals, "date": dates, "time": times})
 df.to_excel("trip_data.xlsx", index=False)
