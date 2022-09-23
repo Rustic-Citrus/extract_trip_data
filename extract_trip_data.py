da7a5404ba049f63d0f62f2915cf14fb0d9ee43e
Rustic-Citrus <mrcurtis.english@gmail.com>
2022-09-23 07:38:41 -0300

    commit

--- extract_trip_data.py
+++ extract_trip_data.py
@@ -6,6 +6,7 @@ import pandas as pd
 totals = []
 dates = []
 times = []
+num_strings_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 for file in glob.glob("./*.pdf"):
     raw = parser.from_file(file)
@@ -13,9 +14,12 @@ for file in glob.glob("./*.pdf"):
     total = total.replace(",", ".")
     total = float(total)
     totals.append(total)
-    datetime_object = datetime.strptime(raw["content"][(raw["content"].find("2022") - 6):(raw["content"].find("2022") + 10)].strip(),"%d/%m/%Y %H:%M")
+    if raw["content"][(raw["content"].find("2022")) - 6] in num_strings_list:
+        datetime_object = datetime.strptime(raw["content"][(raw["content"].find("2022") - 6):(raw["content"].find("2022") + 10)].strip(),"%d/%m/%Y %H:%M")
+    else:
+        datetime_object = datetime.strptime(raw["content"][(raw["content"].find("2022") - 5):(raw["content"].find("2022") + 10)].strip(),"%d/%m/%Y %H:%M")
     dates.append(datetime_object.strftime("%d/%m/%Y"))
     times.append(datetime_object.strftime("%H:%M"))
 
 df = pd.DataFrame({"total": totals, "date": dates, "time": times})
-df.to_excel("uber_trip_data.xlsx", index=False)
+df.to_excel("trip_data.xlsx", index=False)
